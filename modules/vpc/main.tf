459192170e67564e3beaa1b4955307ca9da27a8f
sberts <scberts@gmail.com>
2024-08-31 19:27:12 -0700

    added security group for vpc endpoints

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,6 +3,12 @@ locals {
   app_subnets    = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
   public_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
   db_subnets     = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 8)]
+  https_port = 80
+  tcp_protocol = "tcp"
+  any_port = 0
+  any_protocol = "-1"
+
+  all_ips = ["0.0.0.0/0"]
 }
 
 data "aws_availability_zones" "available" {
@@ -99,11 +105,37 @@ resource "aws_route_table_association" "db" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_security_group" "vpc_endpoint" {
+  name = "vpc endpoint"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "allow_https_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.vpc_endpoint.id
+
+  from_port = local.https_port
+  to_port   = local.https_port
+  protocol  = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.vpc_endpoint.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.us-west-2.ssm"
   vpc_endpoint_type = "Interface"
   subnet_ids        = aws_subnet.app[*].id
+  security_group_ids = [aws_security_group.vpc_endpoint.id]
 
   private_dns_enabled = true
 
@@ -129,6 +161,7 @@ resource "aws_vpc_endpoint" "ec2messages" {
   subnet_ids        = aws_subnet.app[*].id
   service_name      = "com.amazonaws.us-west-2.ec2messages"
   vpc_endpoint_type = "Interface"
+  security_group_ids = [aws_security_group.vpc_endpoint.id]
 
   private_dns_enabled = true
   tags = {
@@ -141,6 +174,7 @@ resource "aws_vpc_endpoint" "ssmmessages" {
   subnet_ids        = aws_subnet.app[*].id
   service_name      = "com.amazonaws.us-west-2.ssmmessages"
   vpc_endpoint_type = "Interface"
+  security_group_ids = [aws_security_group.vpc_endpoint.id]
 
   private_dns_enabled = true
   tags = {
